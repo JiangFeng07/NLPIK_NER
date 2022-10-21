2898d2e94c5f186a529e395852d265b868fc601d
JiangFeng07 <1154142410@qq.com>
2022-10-21 09:19:05 +0800

    update

--- models/bert_crf.py
+++ models/bert_crf.py
@@ -3,7 +3,7 @@
 # @Time  : 2022/10/20 10:51
 # @Author: lionel
 import torch
-from torch import  nn
+from torch import nn
 from torchcrf import CRF
 from transformers import BertModel
 
@@ -20,7 +20,7 @@ class Bert_CRF(nn.Module):
         self.crf = CRF(num_classes, batch_first=True)
 
     def forward(self, input_ids, token_type_ids, attention_mask, y=None):
-        outputs =self.bert_model(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)[0]
+        outputs = self.bert_model(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)[0]
         outputs = self.dropout(outputs)
         outputs = self.fc(outputs)
         logits = self.softmax(outputs)
