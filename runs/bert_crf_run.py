d6d84cedde185ae09898dfc1d14ce0e44f18cbb2
JiangFeng07 <1154142410@qq.com>
2022-11-11 13:57:04 +0800

    update

--- runs/bert_crf_run.py
+++ runs/bert_crf_run.py
@@ -14,7 +14,7 @@ from transformers import AdamW, get_linear_schedule_with_warmup, BertModel
 from datas.msra import MsraNerDataset
 from datas.utils import build_vocab
 from models.bert_crf import Bert_CRF
-from runs.utils import tokenizer, entity_decode
+from runs.utils import tokenizer, entity_decode, metric
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -31,36 +31,6 @@ def collate_fn(batch, vocab2id, label2id):
             gold_labels[i][j] = label2id[ele]
 
     return token_ids, token_type_ids, attention_mask, gold_labels, texts, labels, char_lens
-
-
-def metric(dataloder, model, id2label):
-    correct_num, predict_num, gold_num = 0, 0, 0
-    with tqdm(total=len(dataloder), desc='模型验证进度条') as pbar:
-        for index, batch in enumerate(dataloder):
-            token_ids, token_type_ids, attention_mask, _, texts, gold_labels, char_lens = batch
-            pred_labels = model(token_ids, token_type_ids, attention_mask, None)
-
-            for chars, pred_label, gold_label, char_len in zip(texts, pred_labels, gold_labels, char_lens):
-                pred_label = [id2label[_label] for _label in pred_label[:char_len]]
-                pred_entities = entity_decode(chars, pred_label, mode='BIOES')
-                gold_entities = entity_decode(chars, gold_label, mode='BIOES')
-                pred_entity_set = set()
-                gold_entity_set = set()
-                for key, val in pred_entities.items():
-                    pred_entity_set.add((key, val))
-                for key, val in gold_entities.items():
-                    gold_entity_set.add((key, val))
-                predict_num += len(pred_entity_set)
-                gold_num += len(gold_entity_set)
-                correct_num += len(pred_entity_set & gold_entity_set)
-            pbar.update()
-
-    print("correct_num: {:3d}, predict_num: {:3d}, gold_num: {:3d}".format(correct_num, predict_num, gold_num))
-    precision = correct_num / (predict_num + 1e-10)
-    recall = correct_num / (gold_num + 1e-10)
-    f1_score = 2 * precision * recall / (precision + recall + 1e-10)
-    print('f1: {:4.4f}, precision: {:4.4f}, recall: {:4.4f}'.format(f1_score, precision, recall))
-    return precision, recall, f1_score
 
 
 def train():
--- runs/bilstm_crf_run.py
+++ runs/bilstm_crf_run.py
@@ -6,12 +6,13 @@ import os.path
 
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils import data
-from torchmetrics import Accuracy, Precision, Recall, F1Score
 from tqdm import tqdm
 from datas.msra import MsraNerDataset
 from datas.utils import build_vocab
 from models.bilstm_crf import BILSTM_CRF
 import torch
+
+from runs.utils import metric
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -39,13 +40,13 @@ def collate_fn(batch, vocab2id, label2id):
     return label_list, reviews, seq_len, masks
 
 
-def run():
+def train():
     """实体识别模型——BILstm+crf训练"""
     train_dataset = MsraNerDataset(os.path.join(args.file_path, 'train.json'), max_len=500)
     valid_dataset = MsraNerDataset(os.path.join(args.file_path, 'dev.json'), max_len=500)
     vocab2id, id2vocab = build_vocab(os.path.join(args.file_path, 'vocab.txt'))
     label2id = {'O': 0, 'B-PER': 1, 'I-PER': 2, 'B-ORG': 3, 'I-ORG': 4, 'B-LOC': 5, 'I-LOC': 6}
-
+    id2label = {val: key for key, val in label2id.items()}
     train_loader = data.DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size,
                                    collate_fn=lambda ele: collate_fn(ele, vocab2id, label2id))
     valid_loader = data.DataLoader(valid_dataset, batch_size=args.batch_size,
@@ -68,36 +69,16 @@ def run():
                 loss.backward()
                 optimizer.step()
         model.eval()
-        with tqdm(total=len(train_loader), desc='模型验证进度条') as pbar, torch.no_grad():
-            y_pre, y = [], []
-            for batch_idx, batch in enumerate(valid_loader):
-                label_list, reviews, seq_len, masks = batch
-                prediction = model(reviews, seq_len, None, mask=masks)
-                for m in range(len(prediction)):
-                    for n in range(len(prediction[m])):
-                        y_pre.append(prediction[m][n])
-                        y.append(label_list[m][n].item())
-                pbar.update()
-            precision = Precision(num_classes=len(train_dataset.label_dict)).to(device)
-            recall = Recall(num_classes=len(train_dataset.label_dict)).to(device)
-            f1_score = F1Score(num_classes=len(train_dataset.label_dict)).to(device)
-            y_pre = torch.tensor(y_pre, device=device)
-            y = torch.tensor(y, device=device)
-            precision = precision(y_pre, y)
-            recall = recall(y_pre, y)
-            f1_score = f1_score(y_pre, y)
-            print('Epoch [{}/{}], Valid | P:{:.3f}, R:{: >.3f}, F1:{: >.3f}\n'.
-                  format(epoch + 1, args.epochs, precision.item(), recall.item(), f1_score.item()))
-
+        with torch.no_grad():
+            precision, recall, f1_score = metric(valid_loader, model, id2label)
             if f1_score > best_f1_score:
-                torch.save(model.state_dict(), os.path.join(args.file_path, 'bilstm_ner.pt'))
+                torch.save(model.state_dict(), args.model_path)
                 best_f1_score = f1_score
                 early_epochs = 0
             else:
                 early_epochs += 1
 
-            # 连续三个epoch，验证集f1_score没有提升，训练结束
-            if early_epochs > 2:
+            if early_epochs > 3:  # 连续三个epoch，验证集f1_score没有提升，训练结束
                 print('验证集f1_score连续三个epoch没有提升，训练结束')
                 break
         print('\n')
@@ -117,4 +98,4 @@ if __name__ == '__main__':
     parser.add_argument('--lr', help='学习率', type=float, default=1e-3)
     args = parser.parse_args()
 
-    run()
+    train()
