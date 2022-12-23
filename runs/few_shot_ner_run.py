4549d32917486594a48ca96a6d7b8fc4b2b0971b
JiangFeng07 <1154142410@qq.com>
2022-12-23 14:36:46 +0800

    update

--- runs/few_shot_ner_run.py
+++ runs/few_shot_ner_run.py
@@ -3,17 +3,16 @@
 # @Time  : 2022/11/30 00:04
 # @Author: lionel
 import argparse
-import json
 import os.path
 
+import torch
 from torch.utils import data
 from tqdm import tqdm
-from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+from transformers import BertModel, AdamW, get_linear_schedule_with_warmup
 
 from datas.msra import MsraNerDataset
 from models.few_shot_ner import LabelSemanticsFSNER
-from models.utils import entity_decode, text_to_chars, entity_label_encode, tokenizer, build_vocab
-import torch
+from models.utils import tokenizer, build_vocab, entity_decode
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -26,7 +25,36 @@ def collate_fn(batch, vocab2id, label2id):
     for i, label in enumerate(labels):
         for j, ele in enumerate(label):
             gold_labels[i][j] = label2id[ele]
-    return token_ids, attention_mask, gold_labels
+    return token_ids, attention_mask, gold_labels, chars, labels
+
+
+def metric(dataloader, model, id2label, epoch):
+    correct_num, predict_num, gold_num = 0, 0, 0
+    with tqdm(total=len(dataloader), desc='Epoch：%d，模型训练进度条' % (epoch + 1)) as pbar:
+        for batch_idx, batch in enumerate(dataloader):
+            token_ids, token_attention_mask, _, chars, labels = batch
+            logits = model(token_ids, token_attention_mask)
+            pred_labels = torch.argmax(logits, dim=-1)
+            for char, pred_label, label in zip(chars, pred_labels, labels):
+                pred_label = [id2label[int(_label)] for _label in pred_label[:len(char)]]
+
+                pred_entity_set = set()
+                for key, val in entity_decode(char, pred_label).items():
+                    pred_entity_set.add((key, val['value']))
+                gold_entity_set = set()
+                for key, val in entity_decode(char, label).items():
+                    gold_entity_set.add((key, val['value']))
+                predict_num += len(pred_entity_set)
+                gold_num += len(gold_entity_set)
+                correct_num += len(pred_entity_set & gold_entity_set)
+            pbar.update()
+
+    print("correct_num: {:3d}, predict_num: {:3d}, gold_num: {:3d}".format(correct_num, predict_num, gold_num))
+    precision = correct_num / (predict_num + 1e-10)
+    recall = correct_num / (gold_num + 1e-10)
+    f1_score = 2 * precision * recall / (precision + recall + 1e-10)
+    print('f1: {:4.4f}, precision: {:4.4f}, recall: {:4.4f}'.format(f1_score, precision, recall))
+    return precision, recall, f1_score
 
 
 def train():
@@ -56,7 +84,7 @@ def train():
         model.train()
         with tqdm(total=len(train_dataloader), desc='Epoch：%d，模型训练进度条' % (epoch + 1)) as pbar:
             for batch_idx, batch in enumerate(train_dataloader):
-                token_ids, token_attention_mask, gold_labels = batch
+                token_ids, token_attention_mask, gold_labels, _, _ = batch
                 optimizer.zero_grad()
                 logits = model(token_ids, token_attention_mask)
                 gold_labels = token_attention_mask * gold_labels
@@ -66,6 +94,21 @@ def train():
                 loss.backward(retain_graph=True)
                 optimizer.step()
                 scheduler.step()
+                break
+        with torch.no_grad():
+            model.eval()
+            precision, recall, f1_score = metric(valid_dataloader, model, id2label, epoch)
+            if f1_score > best_f1_score:
+                torch.save(model.state_dict(), args.model_path)
+                best_f1_score = f1_score
+                early_epochs = 0
+            else:
+                early_epochs += 1
+
+            if early_epochs > 3:  # 连续三个epoch，验证集f1_score没有提升，训练结束
+                print('验证集f1_score连续三个epoch没有提升，训练结束')
+                break
+        break
 
 
 if __name__ == '__main__':
